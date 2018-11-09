18bf7406887fb6f322b4477cd41ffc2c699627b3
Yerlan <bayekeshov@gmail.com>
2018-11-09 10:04:50 -0500

    Changed left box logic

--- src/components/button.js
+++ src/components/button.js
@@ -1,28 +1,32 @@
 import React, {Component, Fragment} from 'react';
 
 class Button extends Component {
-	
+		
+	state = {
+		description: ["Many practices can support the function of organisms and build soil health. They are often used individually to address specific soil and plant problems, but the more that are used, the greater the effect on soil health. Explore the practices on this farm — and the organisms below the surface — to learn how they can improve soil health.", "There are many ways that healthy soil supports the growth of high-yielding, high-quality and healthy crops. The benefits range from better management of nutrients and pests to structural changes in the soil that improve water capture and storage. All of these benefits can lead to higher farm profitability. Explore the practices on this farm — and the organisms below the surface — to learn how they help improve soil health.", "Organisms living in the soil play important roles in promoting a healthier soil ecosystem and more resilient plants. Increasing organic matter allows for more activity and diversity of life in the soil, which in turn stimulates soil processes that lead to healthy plants. Explore the practices on this farm — and the organisms below the surface — to learn how they can improve soil health."]
+	}
 
 	onClick(e) {
 		let button = e.target;
 		let panels = document.getElementsByClassName('sidebar-panel');
 		let box = document.getElementsByClassName('desc_frame')[0];
+		let btnId = e.target.dataset.id;
 		for(let i=0; i < panels.length; i++){
 			panels[i].classList.remove('active')
 		}
 		button.nextSibling.classList.add('active')
-		switch(e.target.dataset.id) {
-			case '1':
+		switch(btnId) {
+			case '0':
 				box.classList = 'desc_frame purple';
-				box.firstChild.innerText = "Many practices can support the function of organisms and build soil health. They are often used individually to address specific soil and plant problems, but the more that are used, the greater the effect on soil health. Explore the practices on this farm — and the organisms below the surface — to learn how they can improve soil health.";
+				box.firstChild.innerText = this.state.description[btnId];
+			break;
+			case '1':
+				box.classList = 'desc_frame red';
+				box.firstChild.innerText = this.state.description[btnId];
 			break;
 			case '2':
-				box.classList = 'desc_frame red';
-				box.firstChild.innerText = "There are many ways that healthy soil supports the growth of high-yielding, high-quality and healthy crops. The benefits range from better management of nutrients and pests to structural changes in the soil that improve water capture and storage. All of these benefits can lead to higher farm profitability. Explore the practices on this farm — and the organisms below the surface — to learn how they help improve soil health.";
-			break;
-			case '3':
 				box.classList = 'desc_frame blue';
-				box.firstChild.innerText = "Organisms living in the soil play important roles in promoting a healthier soil ecosystem and more resilient plants. Increasing organic matter allows for more activity and diversity of life in the soil, which in turn stimulates soil processes that lead to healthy plants. Explore the practices on this farm — and the organisms below the surface — to learn how they can improve soil health.";
+				box.firstChild.innerText = this.state.description[btnId];
 			break;
 			default:
 			break;
@@ -34,7 +38,7 @@ class Button extends Component {
 				<Fragment>
 					<button 
 						className="sidebar-btn" 
-						onClick={this.onClick} 
+						onClick={this.onClick.bind(this)} 
 						style={{borderColor:this.props.color, color: this.props.color}}
 						data-id={this.props.id}
 					>{
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -6,9 +6,9 @@ class SideBar extends Component {
 	render() {
 		return(
 				<div>
-					<Button name={'Management Practices'} color={'#883760'} id={'1'}/>
-					<Button name={'Soil Health Benefits'} color={'#ab161b'} id={'2'}/>
-					<Button name={'Soil Organism Functions'} color={'#4387be'} id={'3'}/>
+					<Button name={'Management Practices'} color={'#883760'} id={'0'}/>
+					<Button name={'Soil Health Benefits'} color={'#ab161b'} id={'1'}/>
+					<Button name={'Soil Organism Functions'} color={'#4387be'} id={'2'}/>
 				</div>
 			)
 	}
